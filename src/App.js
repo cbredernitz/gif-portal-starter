46e8051cbdf79fe996c918d44f3d65e6a8166954
Chris <chrisrbredernitz@gmail.com>
2022-08-19 15:43:36 -0600

    added wallet connection to webpage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+import React, { useEffect } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
+
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom Wallet Found!');
+        }
+      } else {
+        alert('Solana object not found. Install a phantom wallet!');
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  };
+
+  /*
+   * When our component first mounts, let's check to see if we have a connected
+   * Phantom Wallet
+   */
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
